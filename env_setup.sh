fe0046be5e48ce47db67dea981e3821eec5569b9
Pedro Bernáldez <pedrobertorres@gmail.com>
2019-03-21 17:36:06 +0100

    URLs of the audios module and models restrictions
    Fixed urls.py of the audios module and models restrictions.

---
+++ env_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Local environment setup script for SoundGo
+
+################### INSTALL REQUIREMENTS ###################
+
+echo -e "\n<<<< Installing requirements >>>>\n"
+
+pip3 install -r requirements.txt
+
+cd soundgo_api
+
+###################### MAKE MIGRATIONS #####################
+
+echo -e "\n<<<< Making migrations >>>>\n"
+
+python3 manage.py makemigrations
+
+########################## MIGRATE #########################
+
+echo -e "\n<<<< Applying migrations  >>>>\n"
+
+python3 manage.py migrate
--- soundgo_api/audios/models.py
+++ soundgo_api/audios/models.py
@@ -1,19 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
-
-# Create your models here.
-
+from django.core.validators import MinValueValidator
 
 
 class Record(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     numberReproductions = models.IntegerField(validators=[MinValueValidator(0)])
-    path = models.CharField(blank=True)
-
+    path = models.CharField(blank=True, max_length=200)
 
 
 class Audio(Record):
@@ -21,7 +14,7 @@ class Audio(Record):
     timestampCreation = models.DateField(auto_now_add=True)
     timestampFinish = models.DateField()
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    site = models.ForeignKey("Site", blank=True, null=True, related_name='records')
+    site = models.ForeignKey("Site", blank=True, null=True, related_name='records', on_delete=models.CASCADE)
     tags = models.ManyToManyField("Tag")
 
 
@@ -33,19 +26,17 @@ class Advertisement(Record):
 
 
 class Category(models.Model):
-    name = models.CharField(blank=True)
+    name = models.CharField(blank=True, max_length=200)
     maxTimeRecord = models.FloatField()
     minDurationMap = models.FloatField()
 
 
 class Tag(models.Model):
-    name = models.CharField(blank=True)
+    name = models.CharField(blank=True, max_length=200)
 
 
 class Site(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
-    name = models.CharField(blank=True)
-    description = models.CharField(blank=True)
-
-
+    name = models.CharField(blank=True, max_length=200)
+    description = models.CharField(blank=True, max_length=200)
