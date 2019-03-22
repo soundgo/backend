6c25b63d2b521de5d9457ad0fb5ecc451f5b8a02
Pedro Bernáldez <pedrobertorres@gmail.com>
2019-03-22 18:41:25 +0100

    Accounts module
    This commit include the accounts module with the models of UserAccount and Actor.

--- soundgo_api/sites/models.py
+++ soundgo_api/sites/models.py
@@ -6,3 +6,8 @@ class Site(models.Model):
     longitude = models.FloatField()
     name = models.CharField(blank=False, max_length=200)
     description = models.CharField(blank=False, max_length=200)
+
+    class Meta:
+        db_table = 'site'
+        verbose_name = 'Site'
+        verbose_name_plural = 'Sites'
--- soundgo_api/tags/models.py
+++ soundgo_api/tags/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(blank=False, max_length=200)
+    name = models.CharField(blank=False, max_length=200, unique=True)
+
+    class Meta:
+        db_table = 'tag'
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
