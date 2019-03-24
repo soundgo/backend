c84e8454219bee2df8db7dfd54ee75d5b5158b8b
Pedro Bernáldez <pedrobertorres@gmail.com>
2019-03-24 12:32:56 +0100

    [CORS] Allow all hosts set to False

--- soundgo_api/soundgo_api/settings.py
+++ soundgo_api/soundgo_api/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
     '127.0.0.1:8000',
