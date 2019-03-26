13125d11ffd1de8055c9d8cadc849ee37e5ca913
Pedro Bernáldez <pedrobertorres@gmail.com>
2019-03-27 00:51:43 +0100

    Recording duration
    This commit includes a new method for the Cloudinary manager that retrieve the records duration.

--- soundgo_api/records/admin.py
+++ soundgo_api/records/admin.py
@@ -7,6 +7,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
     actions = []
 
+    def has_add_permission(self, request):
+        return False
+
     def has_delete_permission(self, request, obj=None):
         return False
 
