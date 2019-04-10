9694da0eac3ef56760e50c40428fce062272705f
Pedro Bernáldez <pedrobertorres@gmail.com>
2019-04-11 01:29:23 +0200

    Fix Django settings in Clock

--- soundgo_api/sites/admin.py
+++ soundgo_api/sites/admin.py
@@ -1,19 +1,9 @@
 from django.contrib import admin
 
 from .models import Site
-from accounts.models import Actor
 
 
 class SiteAdmin(admin.ModelAdmin):
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-
-        if db_field.name == 'actor':
-
-            kwargs['initial'] = Actor.objects.filter(user_account=request.user).all()[0]
-            kwargs['disabled'] = True
-
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
 
