2d9570385166523e1dd60170d25bb6a9cb3530d6
Pedro Bernáldez <pedrobertorres@gmail.com>
2019-04-10 19:25:57 +0200

    CRON job to deleted past audios

--- soundgo_api/soundgo_api/clock.py
+++ soundgo_api/soundgo_api/clock.py
@@ -1,8 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from records.models import Advertisement
+from records.models import Audio, Advertisement
 from accounts.models import CreditCard
 from sites.models import Site
+
+import datetime
 
 scheduler = BlockingScheduler()
 
@@ -15,6 +17,7 @@ def delete_marked_advertisements_job():
 
     """
     CRON job to delete the advertisements marked for deletion.
+    TRIGGER: Last day of each month at 1 AM.
     """
 
     Advertisement.objects.filter(isDelete=True).delete()
@@ -25,6 +28,7 @@ def delete_marked_credit_card_job():
 
     """
     CRON job to delete the credit cards marked for deletion.
+    TRIGGER: Last day of each month at 2 AM.
     """
 
     credit_cards = CreditCard.objects.filter(isDelete=True)
@@ -42,9 +46,21 @@ def reactivate_inactive_advertisements_job():
 
     """
     CRON job to reactivate the inactive advertisements.
+    TRIGGER: Last day of each month at 3 AM.
     """
 
     Advertisement.objects.filter(isActive=False).update(isActive=True)
+
+
+@scheduler.scheduled_job('cron', id='delete_past_audios_job_id', day_of_week='wed', hour=5)
+def delete_past_audios_job():
+
+    """
+    CRON job to delete past audios.
+    TRIGGER: Each wednesday of each week of each month at 5 AM.
+    """
+
+    Audio.objects.filter(timestampFinish__lt=datetime.datetime.now()).delete()
 
 
 scheduler.start()
